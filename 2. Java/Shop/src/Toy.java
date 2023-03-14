b07b62a78ebd5d78e494eda2c2c0dcfedcf40c5b
egorov.dmitriy.an <egorov.dmitriy.an@yandex.ru>
2023-03-15 02:21:54 +0300

    1

---
+++ 2. Java/Shop/src/Toy.java
@@ -0,0 +1,13 @@
+public class Toy {
+    int id_toy;
+    String name_toy;
+    int count;
+    int weight;
+
+    public Toy(int id_toy, String name_toy, int count, int weight) {
+        this.id_toy = id_toy;
+        this.name_toy = name_toy;
+        this.count = count;
+        this.weight = weight;
+    }
+}
